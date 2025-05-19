ce39849807d3787b3cde7fb0254ebc0117508cf6
JoseP <josealejandro_pilavizuete@hotmail.com>
2025-05-19 15:40:53 -0500

    UPDATE 19-5

--- Clase_Proyecto/backend-men/src/database.js
+++ Clase_Proyecto/backend-men/src/database.js
@@ -5,10 +5,10 @@ import moongose from 'mongoose'
 
 moongose.set('strictQuery', true) //Establece que la consulta sea estricta que sea todos los campos de la coleccion
 
-console.log(process.env.MONGO_URI_LOCAL)
+
 const connection =async ()=>{
     try{
-        const connection = await moongose.connect(process.env.MONGO_URI_LOCAL)
+        const connection = await moongose.connect(process.env.MONGODB_URI_LOCAL)
         console.log("Database is connected")
     }catch(e){
         console.log(e)
