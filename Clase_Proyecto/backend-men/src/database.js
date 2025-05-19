80ccc6ee0df957ce77e5925aaf4051c136777cad
JoseP <josealejandro_pilavizuete@hotmail.com>
2025-05-19 15:24:28 -0500

    UPDATE 19-5

--- Clase_Proyecto/backend-men/src/database.js
+++ Clase_Proyecto/backend-men/src/database.js
@@ -1,6 +1,11 @@
-import moongose from 'mongoose'
-moongose.set('strictQuery', true)
+//NoSQL => MongoDB --> Para usar la base de datos y crear colecciones y documentos
+// ODM => Mongoose --> Para poder implementarlo desde el servidor
 
+import moongose from 'mongoose' 
+
+moongose.set('strictQuery', true) //Establece que la consulta sea estricta que sea todos los campos de la coleccion
+
+console.log(process.env.MONGO_URI_LOCAL)
 const connection =async ()=>{
     try{
         const connection = await moongose.connect(process.env.MONGO_URI_LOCAL)
--- Clase_Proyecto/backend-men/src/index.js
+++ Clase_Proyecto/backend-men/src/index.js
@@ -1,7 +1,9 @@
 import app from './server.js'
+import connection from './database.js'
 
 app.listen(app.get('port'),()=>{
     console.log(`Server ok on http://localhost:${app.get('port')}`)
 })
+connection()
 /* sIEMPRE EL PRIMER ARCHIVO QUE SE EJECUTA ES 
   "main": "index.js", */
