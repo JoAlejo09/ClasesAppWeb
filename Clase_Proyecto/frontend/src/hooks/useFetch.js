281b90f62c7dd98c988efe7fba75c1afefdb8ca1
JoseP <josealejandro_pilavizuete@hotmail.com>
2025-05-22 14:52:32 -0500

    TAREA UPDATE

--- Clase_Proyecto/frontend/src/hooks/useFetch.js
+++ Clase_Proyecto/frontend/src/hooks/useFetch.js
@@ -1,28 +1,23 @@
 import axios from 'axios'
-import { toast } from 'react-toastify'
+import {toast} from 'react-toastify'
 
-function useFetch() {
-
-    const fetchDataBackend = async (url, form = null, method = 'POST') => {
-
-        try {
+function useFetch(){
+    const fetchDataBackend = async (url, form=null,method='POST')=>{
+        try{
             let respuesta
-            if (method === 'POST') {
-                respuesta = await axios.post(url, form)
-            } else if (method === 'GET') {
+            if (method == 'POST'){
+                respuesta = await axios.post(url,form)
+            }else if(method == 'GET'){
                 respuesta = await axios.get(url)
             }
             toast.success(respuesta?.data?.msg)
             return respuesta?.data
-            
-        } catch (error) {
-            toast.error(error.response?.data?.msg)
-            const errorMsg = error.response?.data?.msg || 'Error desconocido';
+        }
+        catch(e){
+              const errorMsg=e.response?.data?.msg || 'Error Desconocido'
             throw new Error(errorMsg);
         }
     }
-
-    return { fetchDataBackend }
+    return {fetchDataBackend}
 }
-
 export default useFetch
